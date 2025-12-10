1d4f84309bc0cd74df77dcc5a6539e3953ed451a
Krishna Kumar <krishsat9937@gmail.com>
2025-12-10 16:50:28 +0100

    Fix loading

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dipasmit1-hash.github.io/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
